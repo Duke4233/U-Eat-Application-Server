24750d9fbb9f4f8905261e8563c76f22f2260efb
matbotW <mathewmcdade@gmail.com>
2019-11-16 18:15:57 -0800

    laying out routes

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var searchRouter = require('./routes/search');
+var restaurantsRouter = require('./routes/restaurants');
+var restaurantRouter = require('./routes/restaurant');
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/search', searchRouter);
+app.use('/restaurants', restaurantsRouter);
+app.use('/restaurant', restaurantRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
