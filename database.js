f4b488c9fba348b780dea2c0fdcceb70b3747349
Mathew McDade <mcdadem@flip2.engr.oregonstate.edu>
2019-11-28 00:25:57 -0800

    anonymized db structure

--- database.js
+++ database.js
@@ -1,8 +1,12 @@
 //TODO: in production environment dbSettings should be moved to a private .env setup file for security.
 var sql = require('mysql');
-var settings = require('./dbConfig_mccoymil');
-//var settings = require('./dbConfig_mcdade_local');
 
-var pool = sql.createPool(settings);
+var pool = sql.createPool({
+	"connectionLimit"	: 10,
+	"host"			: process.env.DB_HOST,
+     "database"		: process.env.DB_DB,
+     "user"			: process.env.DB_USER,
+     "password"		: process.env.DB_PASS
+});
 
 module.exports = pool;
