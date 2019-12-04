8723d4863e3734e4d0b9942bf0ce617cfcb9b95b
matbotW <mathewmcdade@gmail.com>
2019-12-04 00:47:30 -0800

    successfully posted new review using ajax.

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -8,23 +8,22 @@ router.get('/:resid', function(req, res, next)
 {
     if(req.session.loggedin)
 	{
-		db.query("SELECT restaurant.name AS resname, menu_item.imageURL, menu_item.name, menu_item.price, NULL AS username, NULL AS rating, NULL AS review " +
+		db.query("SELECT restaurant.name AS resname, restaurant.id AS resid, menu_item.imageURL, menu_item.name, menu_item.price, NULL AS username, NULL AS rating, NULL AS review " +
 			"FROM restaurant INNER JOIN menu_item ON restaurant.id = menu_item.restaurant_id " +
 			"WHERE restaurant.id = ?;" +
-			"SELECT restaurant.name AS resname, NULL AS imageURL, NULL AS name, NULL AS price,account.username, review.rating, review.review " +
+			"SELECT restaurant.name AS resname, restaurant.id AS resid, NULL AS imageURL, NULL AS name, NULL AS price,account.username, review.rating, review.review " +
 			"FROM restaurant JOIN review on restaurant.id = review.restaurant_id JOIN account on account.id = review.account_id " +
 			"WHERE restaurant.id = ?",
 			[req.params.resid, req.params.resid], function(error, results, fields)
 		{
 			if (error)
 			{
-				console.log(error);
 				res.redirect('/');
 			} else {
-				console.log(results);
 				res.render('menu',
 					{
-						title: results[0].resname,
+						title: results[0][0].resname,
+						resid: results[0][0].resid,
 						loggedin: req.session.loggedin,
 						username: req.session.username,
 						userid: req.session.userid,
@@ -38,8 +37,17 @@ router.get('/:resid', function(req, res, next)
     }
 });
 
-router.post('/:resid/review', function (req, res, next) {
-
+router.post('/review', function (req, res, next) {
+	db.query("INSERT INTO review (restaurant_id, account_id, rating, review) values (?,?,?,?)",
+		[req.body.rid, req.body.aid, req.body.rating, req.body.review],
+		function (error, result) {
+			if (error!=null) {
+				console.log(error);
+				res.send({"status": 500, "error": error, "response": null});
+			} else {
+				res.send({"status": 200, "error": null, "response": result});
+			}
+	});
 });
 
 module.exports = router;
