3a914a2a9d17a34d935cbdb48bcad4baab5860d3
matbotW <mathewmcdade@gmail.com>
2019-12-03 18:38:42 -0800

    finally got a good query to get menu_items and reviews in the same results table

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ var pool = sql.createPool({
 	"host"			: process.env.DB_HOST,
      "database"		: process.env.DB_DB,
      "user"			: process.env.DB_USER,
-     "password"		: process.env.DB_PASS
+     "password"		: process.env.DB_PASS,
 });
 
 module.exports = pool;
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database');
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/:resid', function(req, res, next)
 {
     if(req.session.loggedin)
 	{
-		db.query("SELECT restaurant.name AS resname, menu_item.imageURL, menu_item.name, menu_item.price FROM restaurant INNER JOIN menu_item ON restaurant.id = menu_item.restaurant_id WHERE restaurant.id = ?", [req.params.resid], function(error, results, fields)
+		db.query("SELECT restaurant.name AS resname, menu_item.imageURL, menu_item.name, menu_item.price, NULL AS username, NULL AS rating, NULL AS review " +
+			"FROM restaurant INNER JOIN menu_item ON restaurant.id = menu_item.restaurant_id " +
+			"WHERE restaurant.id = ? " +
+			"UNION " +
+			"SELECT restaurant.name AS resname, NULL AS imageURL, NULL AS name, NULL AS price,account.username, review.rating, review.review " +
+			"FROM restaurant JOIN review on restaurant.id = review.restaurant_id JOIN account on account.id = review.account_id " +
+			"WHERE restaurant.id = ?",
+			[req.params.resid, req.params.resid], function(error, results, fields)
 		{
 			if (error)
 			{
